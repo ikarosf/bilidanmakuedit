eae57726146d0d8f618b52798259064a4fd764ae
ikarosf <3316741951@qq.com>
2020-01-24 15:11:07 +0800

    release version

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, os
+
+# import PySide2
+# dirname = os.path.dirname(PySide2.__file__)
+# plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+# os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+# print(plugin_path)
+
+# if hasattr(sys,'frozen'):
+#     os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
+
 from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PySide2 import QtGui
 from MainWindow import *
@@ -8,6 +18,9 @@ import global_env
 import action_def
 import logging
 import resource_rc
+
+# 打包exe文件用，编程时请注释
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join('.', 'plugins')
 
 
 class MyWindow(QMainWindow, Ui_MainWindow):
@@ -22,7 +35,7 @@ class MyWindow(QMainWindow, Ui_MainWindow):
             reply = QtWidgets.QMessageBox.question(self,
                                                    '将关闭程序',
                                                    "是否保存？",
-                                                   QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No|QtWidgets.QMessageBox.Cancel,
+                                                   QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No | QtWidgets.QMessageBox.Cancel,
                                                    QtWidgets.QMessageBox.Cancel)
             if reply == QtWidgets.QMessageBox.Yes:
                 global_env.keep_data_store()
